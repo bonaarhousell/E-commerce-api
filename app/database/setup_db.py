49943403f567be27dcf5ddef1e10f58f8f0a0d05
bonaarhousell <nurmuliadi024@gmail.com>
2026-07-02 22:31:41 +0700

    Use DATABASE_URL for Railway

--- app/database/setup_db.py
+++ app/database/setup_db.py
@@ -6,16 +6,20 @@ import os
 
 load_dotenv()
 
-DB_URL = URL.create(
-    drivername="postgresql+psycopg",
-    username=os.getenv("DB_USERNAME"),
-    password=os.getenv("DB_PASSWORD"),
-    host=os.getenv("DB_HOST"),
-    port=os.getenv("DB_PORT"),
-    database=os.getenv("DB_NAME")
-)
+database_url = os.getenv("DATABASE_URL")
 
-engine = create_engine(DB_URL)
+if database_url:
+    engine = create_engine(database_url)
+else:
+    db_url = URL.create(
+        drivername="postgresql+psycopg",
+        username=os.getenv("DB_USERNAME"),
+        password=os.getenv("DB_PASSWORD"),
+        host=os.getenv("DB_HOST"),
+        port=os.getenv("DB_PORT"),
+        database=os.getenv("DB_NAME"),
+    )
+    engine = create_engine(db_url)
 
 SessionLocal = sessionmaker(
     bind=engine,
