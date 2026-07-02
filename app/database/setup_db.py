4d6d2223b4fc2895dc8750712527e13300d5d6ee
bonaarhousell <nurmuliadi024@gmail.com>
2026-07-02 22:41:39 +0700

    add print statements

--- app/database/setup_db.py
+++ app/database/setup_db.py
@@ -8,6 +8,8 @@ load_dotenv()
 
 database_url = os.getenv("DATABASE_URL")
 
+print("DATABASE_URL exists:", bool(database_url))
+print("DATABASE_URL value:", database_url[:30] + "..." if database_url else None)
 if database_url:
     engine = create_engine(database_url)
 else:
